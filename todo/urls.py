7809af284235a0ee076feefec4f14cc70789482c
paul <pavelgoncharov56@gmail.com>
2025-12-25 12:37:19 +0000

    update and delete logic added

--- todo/urls.py
+++ todo/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = "todo"
 
 urlpatterns = [
-    # path("", views.ArticleListView.as_view(), name="list"),
-    # path("<int:pk>/", views.ArticleDetailView.as_view(), name="detail"),
     path("", views.TaskListView.as_view(), name="list"),
     path("<int:pk>/", views.TaskDetailView.as_view(), name="detail"),
     path("create/", views.TaskCreateView.as_view(), name="create"),
+    path("<int:pk>/update/", views.TaskUpdateView.as_view(), name="update"),
+    path("<int:pk>/delete/", views.TaskDeleteView.as_view(), name="delete"),
 ]
 
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
@@ -22,3 +21,18 @@ class TaskDetailView(DetailView):
     model = Task
     template_name = 'todo/task_detail.html'
     context_object_name = 'task'
+
+
+class TaskUpdateView(SuccessMessageMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'todo/task_form.html'
+    success_url = reverse_lazy('todo:list')
+    success_message = 'Task updated successfully!'
+
+
+class TaskDeleteView(SuccessMessageMixin, DeleteView):
+    model = Task
+    template_name = 'todo/task_confirm_delete.html'
+    success_url = reverse_lazy('todo:list')
+    success_message = 'Task deleted successfully!'
